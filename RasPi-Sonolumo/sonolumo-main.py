92af41ce761a4cc9efa1716ec21433a643f592c3
nick waytowich <nick waytowich>
2016-12-23 00:23:00 -0500

    fixed bugs, added detection threshold

--- RasPi-Sonolumo/sonolumo-main.py
+++ RasPi-Sonolumo/sonolumo-main.py
@@ -6,9 +6,12 @@ import alsaaudio
 import struct
 import numpy as np
 import numpy as numpy
+import matplotlib.pyplot as plt
 if (os.uname())[1] == 'raspberrypi':
     from Adafruit_PWM_Servo_Driver import PWM
-import matplotlib.pyplot as plt
+    use_sim = False
+else:
+    use_sim = True
 
 
 # Main Class
@@ -24,11 +27,22 @@ class SonoLumo(object):
         # Attributes for USB mic & Signal Processing
         self.use_sim = use_sim
         self.is_raspi = (os.uname())[1] == 'raspberrypi'
-        self.USB_Name = 'hw:CARD=C920' #change name to final USB mic
+        self.USB_Name = 'hw:CARD=Device' #change name to final USB mic
         self.SamplingRate = 16000
         self.Channels = 1
-        self.chunk = 1000
+        self.chunk = 2000
         self.nfft = self.chunk
+        self.maxDetectFreq = 4000
+		self.threshold = 900.0
+        # Open the device in nonblocking capture mode.
+        self.inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL, self.USB_Name)
+
+        # Set attributes for recording:
+        self.inp.setchannels(self.Channels)
+        self.SamplingRate = self.inp.setrate(self.SamplingRate)
+        self.inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+        self.inp.setperiodsize(self.chunk)
+        
         self.freqs = np.linspace(0.0,1.0/(2.0*(1.0/self.SamplingRate)),self.nfft/2.0)
         self.time_resol = (self.nfft*(1.0) / self.SamplingRate*(1.0)) *1000.0
         self.windowF = np.hamming(self.nfft)
@@ -37,18 +51,18 @@ class SonoLumo(object):
         self.maxFreq = 0.0;
     
         # Attributes for RasPi Hat
-        self.Ch_r1 = 1
+        self.Ch_r1 = 0
+        self.Ch_g1 = 1
         self.Ch_b1 = 2
-        self.Ch_g1 = 3
-        self.Ch_r2 = 5
+        self.Ch_r2 = 4
+        self.Ch_g2 = 5
         self.Ch_b2 = 6
-        self.Ch_g2 = 7
-        self.Ch_r3 = 9
+        self.Ch_r3 = 8
+        self.Ch_g3 = 9
         self.Ch_b3 = 10
-        self.Ch_g3 = 11
-        self.Ch_r4 = 13
+        self.Ch_r4 = 12
+        self.Ch_g4 = 13
         self.Ch_b4 = 14
-        self.Ch_g4 = 15
         self.pulse_width = 0.5
         self.num_tics = 4096.0
         
@@ -66,14 +80,6 @@ class SonoLumo(object):
             self.pwm = PWM(0x40)
             self.pwm.setPWMFreq(1000) # set to max frequency
         
-        # Open the device in nonblocking capture mode.
-        self.inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL, self.USB_Name)
-        
-        # Set attributes for recording:
-        self.inp.setchannels(self.Channels)
-        self.inp.setrate(self.SamplingRate)
-        self.inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
-        self.inp.setperiodsize(self.chunk)
         
         
     def __del__(self):
@@ -117,6 +123,7 @@ class SonoLumo(object):
             self.pwm.setPWM(self.Ch_r4,0, (self.ring4_color[0]*self.num_tics).astype(int))
             self.pwm.setPWM(self.Ch_g4,0, (self.ring4_color[1]*self.num_tics).astype(int))
             self.pwm.setPWM(self.Ch_b4,0, (self.ring4_color[2]*self.num_tics).astype(int))
+			
         else: #use simulated flower (for testing and debugging)
             self.ax.add_artist(plt.Circle((0.5, 0.5), self.ring4_sim, color=self.ring4_color))
             self.ax.add_artist(plt.Circle((0.5, 0.5), self.ring3_sim, color=self.ring3_color))
@@ -139,13 +146,21 @@ class SonoLumo(object):
                 # Apply FFT
                 yf = np.fft.rfft(data2)
                 yf = 2.0/self.nfft * np.abs(yf[:self.nfft//2])
-                
+
                 # Find Max Freq
                 self.maxFreq = self.freqs[np.argmax(yf)]
-                                
+                
                 # Convert Frequency to Color
-                rgba = self.colors(self.maxFreq/self.freqs[self.freqs.size-1])
-                                
+                rgba = self.colors(self.maxFreq/self.maxDetectFreq)
+                
+                if(np.max(yf)<self.threshold):
+                    lst = list(rgba)
+                    lst[0] = np.float64(0.99)
+                    lst[1] = np.float64(0.99)
+                    lst[2] = np.float64(0.99)
+                    lst[3] = np.float64(1.0)
+                    rgba = tuple(lst)
+                
                 # Set colors for LED array
                 self.ring4_color = self.ring3_color
                 self.ring3_color = self.ring2_color
@@ -155,10 +170,10 @@ class SonoLumo(object):
                 self.setLEDcolors() # update pwm color values for LED strips
         
                 # short pause (use this to control timing for the color propogation...for now)
-                time.sleep(0.1)
+                time.sleep(0.2)
 
 if __name__ == '__main__':
-    use_sim = True # True -> run simulator, False -> run Flower LED's
+    # use_sim = False # True -> run simulator, False -> run Flower LED's
     sonolumo = SonoLumo(use_sim)
     sonolumo.run()
 
